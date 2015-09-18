0084e32668022347fd9361d0c73d9daa200890e6
Yang.Y <yang@yangyubo.com>
2015-09-18 12:00:57 +0800

    Incorrect deploy target setting

--- gen_libssh_project.sh
+++ gen_libssh_project.sh
@@ -8,9 +8,11 @@ cd $BASE_DIR/openssl
 ./openssl-build.sh
 popd
 
+export 
+
 pushd .
 cd $BASE_DIR/libssh/build/
 # always regenerate project file
 rm -f CMakeCache.txt
-cmake -DCMAKE_PREFIX_PATH=$BASE_DIR/openssl/dist/openssl-1.0.1l-osx -DOPENSSL_ROOT_DIR= -DCMAKE_INSTALL_PREFIX=./dist -DCMAKE_BUILD_TYPE=MinSizeRel -DWITH_SSH1=OFF -DWITH_STATIC_LIB=ON -DWITH_SERVER=ON -MACOSX_DEPLOYMENT_TARGET=10.9 -GXcode ../
+cmake -DCMAKE_PREFIX_PATH=$BASE_DIR/openssl/dist/openssl-1.0.1l-osx -DOPENSSL_ROOT_DIR= -DCMAKE_INSTALL_PREFIX=./dist -DCMAKE_BUILD_TYPE=MinSizeRel -DWITH_SSH1=OFF -DWITH_STATIC_LIB=ON -DWITH_SERVER=ON -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_OSX_ARCHITECTURES=x86_64 -GXcode ../
 popd
