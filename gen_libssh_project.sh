ded9a7552d296ebe79a43a7bdd74ec91fe1d5475
Yang.Y <yang@yangyubo.com>
2015-10-12 21:31:06 +0800

    Updated gen_libssh_project.sh to work with Xcode 7

--- gen_libssh_project.sh
+++ gen_libssh_project.sh
@@ -2,17 +2,27 @@
 
 BASE_DIR=`pwd`
 
-# delete $BASE_DIR/openssl/dist dir to force rebuild openssl
 pushd .
-cd $BASE_DIR/openssl
-./openssl-build.sh
-popd
 
-export 
+BUILD_DIR="$BASE_DIR/libssh/build/"
 
-pushd .
-cd $BASE_DIR/libssh/build/
+cd ${BUILD_DIR}
+
+OPENSSL_VERSION="openssl-1.0.2d"
+
+OPENSSL_FILE="${OPENSSL_VERSION}-osx.tar.gz"
+OPENSSL_URL="https://raw.githubusercontent.com/codinn/prebuilt-openssl/master/dist/${OPENSSL_FILE}"
+
+if [ ! -e "${OPENSSL_FILE}" ]; then
+	curl "${OPENSSL_URL}" -o "${OPENSSL_FILE}"
+else
+   echo "Using ${OPENSSL_FILE}"
+fi
+
+echo "Unpacking OpenSSL"
+tar xfz "${OPENSSL_FILE}"
+
 # always regenerate project file
 rm -f CMakeCache.txt
-cmake -DCMAKE_PREFIX_PATH=$BASE_DIR/openssl/dist/openssl-1.0.1l-osx -DOPENSSL_ROOT_DIR= -DCMAKE_INSTALL_PREFIX=./dist -DCMAKE_BUILD_TYPE=MinSizeRel -DWITH_SSH1=OFF -DWITH_STATIC_LIB=ON -DWITH_SERVER=ON -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_OSX_ARCHITECTURES=x86_64 -GXcode ../
+cmake -DCMAKE_PREFIX_PATH=${BUILD_DIR}/${OPENSSL_VERSION}-osx -DOPENSSL_ROOT_DIR= -DCMAKE_INSTALL_PREFIX=./dist -DCMAKE_BUILD_TYPE=MinSizeRel -DWITH_SSH1=OFF -DWITH_STATIC_LIB=ON -DWITH_SERVER=ON -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_OSX_ARCHITECTURES=x86_64 -GXcode ../
 popd
