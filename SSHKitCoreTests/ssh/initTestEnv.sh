ff96f5ef07ceec482e7f3d37f6e89ac28cfec033
Yang.Y <yang@yangyubo.com>
2016-04-21 14:52:23 +0800

    Also create sshtest-m and sshtest-nopass for multi-factor and password auth disabled scene

--- SSHKitCoreTests/ssh/initTestEnv.sh
+++ SSHKitCoreTests/ssh/initTestEnv.sh
@@ -7,10 +7,38 @@
 #
 
 # https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man1/dscl.1.html
-# create user sshtest
+
+# Create user sshtest for single-factor authentication
 sudo ./osx-adduser.sh sshtest "SSH Test" "v#.%-dzd"
 sudo mkdir -p /Users/sshtest/.ssh
-sudo sh -c "cat ./ssh/ssh_host_rsa_key.pub >> /Users/sshtest/.ssh/authorized_keys"
+sudo sh -c "cat ./ssh_rsa_key.pub >> /Users/sshtest/.ssh/authorized_keys"
 sudo chmod -R 700 /Users/sshtest/.ssh
 sudo chown -R sshtest:staff /Users/sshtest/
-# vim /etc/ssh/sshd_config
+
+# Create user sshtest-m for multi-factor authentication purpose
+sudo ./osx-adduser.sh sshtest-m "SSH Test Multi" "v#.%-dzd"
+sudo mkdir -p /Users/sshtest-m/.ssh
+sudo sh -c "cat ./ssh_rsa_key.pub >> /Users/sshtest-m/.ssh/authorized_keys"
+sudo chmod -R 700 /Users/sshtest-m/.ssh
+sudo chown -R sshtest-m:staff /Users/sshtest-m/
+
+# Create user sshtest-nopass for testing password auth disabled
+sudo ./osx-adduser.sh sshtest-nopass "SSH Test NoPass" "v#.%-dzd"
+sudo mkdir -p /Users/sshtest-nopass/.ssh
+sudo sh -c "cat ./ssh_rsa_key.pub >> /Users/sshtest-nopass/.ssh/authorized_keys"
+sudo chmod -R 700 /Users/sshtest-nopass/.ssh
+sudo chown -R sshtest-m:staff /Users/sshtest-nopass/
+
+echo "
+!! Please copy following text to the end of your /etc/ssh/sshd_config file!!
+
+Match User sshtest
+	PasswordAuthentication yes
+
+Match User sshtest-m
+	PasswordAuthentication yes
+	AuthenticationMethods publickey,keyboard-interactive,password,keyboard-interactive,publickey
+
+Match User sshtest-nopass
+	PasswordAuthentication no
+"
